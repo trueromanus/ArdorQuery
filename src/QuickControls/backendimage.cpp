bcc217cf84698fbdefe07a6fbd4179e184cae827
Roman Vladimirov <trueromanus@gmail.com>
2022-07-01 16:13:55 +0300

    * Remake centered image while scaling

--- src/QuickControls/backendimage.cpp
+++ src/QuickControls/backendimage.cpp
@@ -17,12 +17,28 @@ void BackendImage::setSource(const QImage &image) noexcept
 
 void BackendImage::paint(QPainter *painter)
 {
-    auto bounding_rect = boundingRect();
-    auto scaled = m_source.scaledToHeight(bounding_rect.height());
-    auto center = bounding_rect.center() - scaled.rect().center();
+    if (m_source.height() > height()) {
+        auto bounding_rect = boundingRect();
+        auto scaled = m_source.scaledToHeight(bounding_rect.height());
+        auto center = bounding_rect.center() - scaled.rect().center();
 
-    if(center.x() < 0) center.setX(0);
-    if(center.y() < 0) center.setY(0);
+        if(center.x() < 0) center.setX(0);
+        if(center.y() < 0) center.setY(0);
 
-    painter->drawImage(center, scaled);
+        painter->drawImage(center, scaled);
+        return;
+    }
+    if (m_source.height() < height()) {
+        auto bounding_rect = boundingRect();
+        auto center = bounding_rect.center() - m_source.rect().center();
+
+        if(center.x() < 0) center.setX(0);
+        if(center.y() < 0) center.setY(0);
+
+        painter->drawImage(center, m_source);
+        return;
+    }
+
+    //if original size not need processing
+    painter->drawImage(QPoint(0,0), m_source);
 }
