8bde1f6603bc0b06120bf6a3fa5fbda4c7c98161
Roman Vladimirov <trueromanus@gmail.com>
2023-10-13 20:57:51 +0300

    * remake interface for interacting with pressed keys

--- src/ViewModels/globaleventhandlermodel.cpp
+++ src/ViewModels/globaleventhandlermodel.cpp
@@ -7,6 +7,30 @@ GlobalEventHandlerModel::GlobalEventHandlerModel(QObject *parent)
     : QObject{parent}
 {
     QGuiApplication::instance()->installEventFilter(this);
+
+    m_keyMapping.insert(Qt::Key_Control, m_control);
+    m_keyMapping.insert(Qt::Key_Shift, m_shift);
+    m_keyMapping.insert(Qt::Key_Alt, m_alt);
+
+    m_keyMapping.insert(Qt::Key_Minus, m_minusKey);
+    m_keyMapping.insert(Qt::Key_Underscore, m_minusKey);
+    m_keyMapping.insert(Qt::Key_Plus, m_plusKey);
+    m_keyMapping.insert(Qt::Key_Equal, m_plusKey);
+
+    m_keyMapping.insert(Qt::Key_F1, m_f1Key);
+    m_keyMapping.insert(Qt::Key_F2, m_f2Key);
+    m_keyMapping.insert(Qt::Key_F3, m_f3Key);
+    m_keyMapping.insert(Qt::Key_F4, m_f4Key);
+    m_keyMapping.insert(Qt::Key_F5, m_f5Key);
+    m_keyMapping.insert(Qt::Key_F6, m_f6Key);
+    m_keyMapping.insert(Qt::Key_F7, m_f7Key);
+    m_keyMapping.insert(Qt::Key_F8, m_f8Key);
+    m_keyMapping.insert(Qt::Key_F9, m_f9Key);
+    m_keyMapping.insert(Qt::Key_F10, m_f10Key);
+    m_keyMapping.insert(Qt::Key_F11, m_f11Key);
+    m_keyMapping.insert(Qt::Key_F12, m_f12Key);
+    m_keyMapping.insert(Qt::Key_Tab, m_tabKey);
+    m_keyMapping.insert(Qt::Key_Escape, m_escapeKey);
 }
 
 bool GlobalEventHandlerModel::eventFilter(QObject *watched, QEvent *event)
@@ -29,28 +53,46 @@ bool GlobalEventHandlerModel::eventFilter(QObject *watched, QEvent *event)
     }
 
     auto keyEvent = static_cast<QKeyEvent*>(event);
+    auto button = keyEvent->key();
 
-    if (t == QEvent::KeyPress) {
-        auto button = keyEvent->key();
+    if (!m_keyMapping.contains(button)) return QObject::eventFilter(watched, event);
 
-        if (button == Qt::Key_Control) m_controlPressed = true;
-        if (button == Qt::Key_Shift) m_shiftPressed = true;
-        if (button == Qt::Key_Alt) m_altPressed = true;
+    auto keyName = m_keyMapping.value(button);
+    auto countKeys = m_pressedKeys.count();
 
-        if (button == Qt::Key_Minus || button == Qt::Key_Underscore) emit keyButtonPressed(m_minusKey);
-        if (button == Qt::Key_Plus || button == Qt::Key_Equal) emit keyButtonPressed(m_plusKey);
-        if (button == Qt::Key_F1) emit keyButtonPressed(m_f1Key);
-        if (button == Qt::Key_Tab) emit keyButtonPressed(m_tabKey);
+    if (t == QEvent::KeyPress && !m_pressedKeys.contains(keyName)) m_pressedKeys.insert(keyName);
 
-    }
+    if (t == QEvent::KeyRelease && m_pressedKeys.contains(keyName)) m_pressedKeys.remove(keyName);
 
-    if (t == QEvent::KeyRelease) {
-        auto button = keyEvent->key();
-
-        if (button == Qt::Key_Control) m_controlPressed = false;
-        if (button == Qt::Key_Shift) m_shiftPressed = false;
-        if (button == Qt::Key_Alt) m_altPressed = false;
-    }
+    auto isChanged = countKeys != m_pressedKeys.count();
+    if (isChanged) emit keysChanged(pressedKeysToString());
 
     return QObject::eventFilter(watched, event);
+}
+
+QString GlobalEventHandlerModel::pressedKeysToString()
+{
+    QStringList result;
+    if (m_pressedKeys.contains(m_control)) result.append(m_control);
+    if (m_pressedKeys.contains(m_shift)) result.append(m_shift);
+    if (m_pressedKeys.contains(m_alt)) result.append(m_alt);
+
+    if (m_pressedKeys.contains(m_f1Key)) result.append(m_f1Key);
+    if (m_pressedKeys.contains(m_f2Key)) result.append(m_f2Key);
+    if (m_pressedKeys.contains(m_f3Key)) result.append(m_f3Key);
+    if (m_pressedKeys.contains(m_f4Key)) result.append(m_f4Key);
+    if (m_pressedKeys.contains(m_f5Key)) result.append(m_f5Key);
+    if (m_pressedKeys.contains(m_f6Key)) result.append(m_f6Key);
+    if (m_pressedKeys.contains(m_f7Key)) result.append(m_f7Key);
+    if (m_pressedKeys.contains(m_f8Key)) result.append(m_f8Key);
+    if (m_pressedKeys.contains(m_f9Key)) result.append(m_f9Key);
+    if (m_pressedKeys.contains(m_f10Key)) result.append(m_f10Key);
+    if (m_pressedKeys.contains(m_f11Key)) result.append(m_f11Key);
+    if (m_pressedKeys.contains(m_f12Key)) result.append(m_f12Key);
+    if (m_pressedKeys.contains(m_escapeKey)) result.append(m_escapeKey);
+
+    if (m_pressedKeys.contains(m_plusKey)) result.append(m_plusKey);
+    if (m_pressedKeys.contains(m_minusKey)) result.append(m_minusKey);
+
+    return result.join("-");
 }
--- src/ViewModels/globaleventhandlermodel.h
+++ src/ViewModels/globaleventhandlermodel.h
@@ -2,6 +2,8 @@
 #define GLOBALEVENTHANDLERMODEL_H
 
 #include <QObject>
+#include <QSet>
+#include <QMap>
 
 class GlobalEventHandlerModel : public QObject
 {
@@ -11,20 +13,38 @@ private:
     const QString m_plusKey { "plus" };
     const QString m_minusKey { "minus" };
     const QString m_f1Key { "f1" };
+    const QString m_f2Key { "f2" };
+    const QString m_f3Key { "f3" };
+    const QString m_f4Key { "f4" };
+    const QString m_f5Key { "f5" };
+    const QString m_f6Key { "f6" };
+    const QString m_f7Key { "f7" };
+    const QString m_f8Key { "f8" };
+    const QString m_f9Key { "f9" };
+    const QString m_f10Key { "f10" };
+    const QString m_f11Key { "f11" };
+    const QString m_f12Key { "f12" };
     const QString m_tabKey { "tab" };
-    bool m_controlPressed { false };
-    bool m_shiftPressed { false };
-    bool m_altPressed { false };
+    const QString m_escapeKey { "escape" };
+    const QString m_control { "control" };
+    const QString m_shift { "shift" };
+    const QString m_alt { "alt" };
+    QMap<int, QString> m_keyMapping { QMap<int, QString>() };
+    QSet<QString> m_pressedKeys { QSet<QString>() };
 
 public:
     explicit GlobalEventHandlerModel(QObject *parent = nullptr);
 
     bool eventFilter(QObject* watched, QEvent* event);
 
+private:
+    QString pressedKeysToString();
+
 signals:
     void backButtonPressed();
     void forwardButtonPressed();
-    void keyButtonPressed(const QString& key);
+    void keysChanged(const QString& state);
+
 };
 
 #endif // GLOBALEVENTHANDLERMODEL_H
